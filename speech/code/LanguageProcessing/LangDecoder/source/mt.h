1814b6fc0386e8d2af67fcdfcf0ecbad8e6a8c59
ilya_p <ilya_p@70fd935d-ab2c-0410-87c7-a1c3f06a4c7f>
2007-11-30 07:57:44 +0000

    LangProc - math - introduce

---
+++ speech/code/LanguageProcessing/LangDecoder/source/mt.h
@@ -0,0 +1,21 @@
+#ifndef __mt_h__
+#define __mt_h__
+
+#include <math.h>
+
+namespace mt {
+
+    typedef double Real;
+
+    static const Real EPSILON = 1e-16;
+
+    inline bool isEqual(Real a, Real b)
+    {
+        return abs(a - b) < EPSILON;
+    }
+
+
+
+} // namespace mt
+
+#endif //__mt_h__
